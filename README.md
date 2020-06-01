2f8d744a31e0aabcc0ae4b3cc549cbe3f571b79f
cw chen <sky85245630@gmail.com>
2020-06-02 00:12:30 +0800

    first commit Instant-Prototyping

---
+++ README.md
@@ -0,0 +1,12 @@
+# Instant Prototyping(快速原型開發)
+
+## Project serve
+```
+vue serve '你的檔案名稱'
+```
+
+## Project build
+```
+vue build '你的檔案名稱'
+```
+
---
+++ prototype.vue
@@ -0,0 +1,22 @@
+<template>
+    <div>
+        asd
+        {{mes}}c
+        cccccc
+        <div class="asd">123321</div>
+    </div>
+</template>
+
+<script>
+export default {
+    data(){
+        return{
+            mes:'message'
+        }
+    }
+}
+</script>
+
+<style lang="css">
+    @import url('style.css');
+</style>
